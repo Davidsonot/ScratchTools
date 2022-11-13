4bda9411ea6a60b949e1f39c874841b7d3998917
Davidsonot <118115694+Davidsonot@users.noreply.github.com>
2022-11-13 10:01:01 -0400

    turbowarp on scratch
    turbowarp on scratch is in now yeah

---
+++ features/turbo-packer.js
@@ -0,0 +1,10 @@
+{"turbo":true,"interpolation":true,"highQualityPen":true,"maxClones":9999999999,"fencing":false,"miscLimits":false,"username":
+"davidsonot","closeWhenStopped":true,"framerate":30,"stageWidth":480,"stageHeight":360,"resizeMode":"preserve-ratio","autoplay"
+:false,"projectId":"p4-@pong food.sb3","custom":{"css":"","js":""},"appearance":{"background":"#000000","foreground"
+:"#ffffff","accent":"#ff4c4c"},"loadingScreen":{"progressBar":true,"text":"","imageMode":"normal","image":null},"controls":
+{"greenFlag":{"enabled":false},"stopAll":{"enabled":false},"fullscreen":{"enabled":false},"pause":{"enabled":false}},"monitors"
+:{"editableLists":false,"variableColor":"#ff8c1a","listColor":"#fc662c"},"compiler":{"enabled":true,"warpTimer":false},
+  "packagedRuntime":true,"target":"html","app":{"icon":null,"packageName":"pong-food","windowTitle":"pong food","windowMode":"window","version":"1.0.0"},"chunks":{"gamepad":false,"pointerlock":false},"cloudVariables":{"mode":"ws","cloudHost":"wss://clouddata.turbowarp.org","custom":
+{"☁ Score":"ws"},
+  "specialCloudBehaviors":false,"unsafeCloudBehaviors":false},"cursor":{"type":"auto","
+custom":null,"center":{"x":0,"y":0}},"extensions":[]}
